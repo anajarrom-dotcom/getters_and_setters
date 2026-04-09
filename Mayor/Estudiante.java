1257608339f5e64028e6e8ba46e7b2e5fd2a4ace
Antony Najarro <anajarrom@miumg.edu.gt>
2026-04-08 18:50:07 -0600

    Termometro inteligente

--- Mayor/Estudiante.java
+++ Mayor/Estudiante.java
@@ -2,4 +2,24 @@ package Mayor;
 
 public class Estudiante {
 
+    private String carnet;
+    private String nombre;
+    private double promedio;
+
+    public Estudiante(String carnet, String nombre, double promedio) {
+        this.carnet = carnet;
+        this.nombre = nombre;
+
+        if (promedio >= 0 && promedio <= 100) {
+            this.promedio = promedio;
+        }
+    }
+
+    public boolean esBecado() {
+        if (promedio >= 85) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
