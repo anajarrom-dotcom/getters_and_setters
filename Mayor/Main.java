57fe77e40e1f33fcf2cfc7426de64ca390dd62b4
Antony Najarro <anajarrom@miumg.edu.gt>
2026-04-08 18:20:22 -0600

    Persona

---
+++ Mayor/Main.java
@@ -0,0 +1,15 @@
+package Mayor;
+
+public class Main {
+	public static void main(String[]arsg ) {
+
+		Persona prs1 = new Persona("Antony","Najarro",20);
+		Persona prs2 = new Persona("David","Lopez",-76);
+		Persona prs3 = new Persona("Fernanda","Aguilar",60);
+
+		System.out.println(prs1);
+		System.out.println(prs2);
+		System.out.println(prs3);
+	}
+
+}
